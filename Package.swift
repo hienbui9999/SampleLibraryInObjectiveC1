626439fba4b40ae63feb53241d184d88dd8c7ad4
hienbui9999 <94465107+hienbui9999@users.noreply.github.com>
2022-05-24 18:43:48 +0700

    Add files via upload

---
+++ Package.swift
@@ -0,0 +1,65 @@
+// swift-tools-version:5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+import PackageDescription
+
+let package = Package(
+    name: "FakeGTMSessionFetcher",
+    platforms: [
+        .iOS(.v9),
+        .macOS(.v10_12),
+        .tvOS(.v10),
+        .watchOS(.v6)
+    ],
+    products: [
+        .library(
+            name: "FakeGTMSessionFetcher",
+            targets: ["FakeGTMSessionFetcherCore", "FakeGTMSessionFetcherFull"]
+        ),
+        .library(
+            name: "FakeGTMSessionFetcherCore",
+            targets: ["FakeGTMSessionFetcherCore"]
+        ),
+        .library(
+            name: "FakeGTMSessionFetcherFull",
+            targets: ["FakeGTMSessionFetcherFull"]
+        ),
+        .library(
+            name: "FakeGTMSessionFetcherLogView",
+            targets: ["FakeGTMSessionFetcherLogView"]
+        )
+    ],
+    targets: [
+        .target(
+            name: "FakeGTMSessionFetcherCore",
+            path: "Sources/Core",
+            publicHeadersPath: "Public"
+        ),
+        .target(
+            name: "FakeGTMSessionFetcherFull",
+            dependencies: ["FakeGTMSessionFetcherCore"],
+            path: "Sources/Full",
+            publicHeadersPath: "Public"
+        ),
+        .target(
+            name: "FakeGTMSessionFetcherLogView",
+            dependencies: ["FakeGTMSessionFetcherCore"],
+            path: "Sources/LogView",
+            publicHeadersPath: "Public"
+        )
+        /*.testTarget(
+            name: "FakeGTMSessionFetcherCoreTests",
+            dependencies: ["FakeGTMSessionFetcherFull", "FakeGTMSessionFetcherCore"],
+            path: "UnitTests"
+        ),
+        
+        .testTarget(
+            name: "objc-import-test",
+            dependencies: [
+                "FakeGTMSessionFetcherCore",
+                "FakeGTMSessionFetcherFull",
+                "FakeGTMSessionFetcherLogView",
+            ],
+            path: "SwiftPMTests/ObjCImportTest"
+        ),*/
+    ]
+)
