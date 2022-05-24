906b9da900b8b75e2accb7f40c2e63cc1f7958f9
hienbui9999 <buiduyhien@gmail.com>
2022-05-24 15:44:25 +0700

    Initial Commit

---
+++ SampleLibraryInObjectiveC1/SampleLibraryInObjectiveC1.h
@@ -0,0 +1,12 @@
+//
+//  SampleLibraryInObjectiveC1.h
+//  SampleLibraryInObjectiveC1
+//
+//  Created by Hien on 24/05/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SampleLibraryInObjectiveC1 : NSObject
+
+@end
---
+++ SampleLibraryInObjectiveC1/SampleLibraryInObjectiveC1.m
@@ -0,0 +1,12 @@
+//
+//  SampleLibraryInObjectiveC1.m
+//  SampleLibraryInObjectiveC1
+//
+//  Created by Hien on 24/05/2022.
+//
+
+#import "SampleLibraryInObjectiveC1.h"
+
+@implementation SampleLibraryInObjectiveC1
+
+@end
